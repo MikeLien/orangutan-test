332ad03a6c64bd600b81a64f34133bcaf617c202
Al Tsai[:atsai] <atsai@mozilla.com>
2014-08-12 16:35:20 +0800

    ready for testing

--- lib/argparser.py
+++ lib/argparser.py
@@ -11,11 +11,16 @@ class Parser(object):
     @staticmethod
     def parser(input):
         parser = argparse.ArgumentParser(description='Orangutan Test Tool by TWQA')
-        parser.add_argument('--config', help='repo of config file')
-        parser.add_argument('--output-folder', help='repo for saving logs')
-        parser.add_argument('--gen-scripts-amount', help='the amount of scripts')
-        parser.add_argument('--gen-scripts-steps', help='the steps in a script')
-        parser.add_argument('--gen-scripts-output', help='the repo for scripts')
+        parser.add_argument('--config',
+                            help='repo of config file')
+        parser.add_argument('--output-folder',
+                            default='', help='repo for saving logs')
+        parser.add_argument('--gen-scripts-amount',
+                            default=0, help='the amount of scripts')
+        parser.add_argument('--gen-scripts-steps',
+                            default=0, help='the steps in a script')
+        parser.add_argument('--gen-scripts-output',
+                            default='', help='the repo for scripts')
         options = parser.parse_args(input)
         return options
 
